e88f200a1f3c53eeaaa598b244337cea548bf763
Florian Wiedenbein <florian.wiedenbein@yahoo.de>
2025-08-11 12:02:49 +0200

    first project init
    first project init

---
+++ Assets/Scripts/Player_IdleState.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Player_IdleState : Player_GroundedState
+{
+    public Player_IdleState(Player player, StateMachine stateMachine, string animBoolName) : base(player, stateMachine, animBoolName)
+    {
+        
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (player.moveInput.x != 0)
+        {
+            stateMachine.ChangeState(player.moveState);
+        }
+    }
+}
